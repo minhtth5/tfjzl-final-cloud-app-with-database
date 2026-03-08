a6e0e684126dfea1d173b9ed102fa6193a095e7a
Minh <minhtth5@fpt.edu.vn>
2026-03-08 16:25:29 +0700

    Fix bugs

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -149,9 +149,14 @@ def show_exam_result(request, course_id, submission_id):
     choices = submission.choices.all()
     total_score = 0 
     questions = course.question_set.all() # Assuming course has related questions
-    for question in questions: correct_choices = question.choice_set.filter(is_correct=True) # Get all correct choices for the question selected_choices = choices.filter(question=question) # Get the user's selected choices for the question
-    # Check if the selected choices are the same as the correct choices if set(correct_choices) == set(selected_choices): total_score += question.grade # Add the question's grade only if all correct answers are selected
-
+    for question in questions: correct_choices = question.choice_set.filter(is_correct=True) 
+    # Get all correct choices for the question 
+    selected_choices = choices.filter(question=question)
+    # Get the user's selected choices for the question
+    # Check if the selected choices are the same as the correct choices 
+    if set(correct_choices) == set(selected_choices): total_score += question.grade 
+    
+    # Add the question's grade only if all correct answers are selected
     context['course'] = course 
     context['grade'] = total_score 
     context['choices'] = choices
